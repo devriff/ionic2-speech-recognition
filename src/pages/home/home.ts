40cad02388dbac44a4486197b1e1dca241ce504d
TheDevShredder <ragdev357@gmail.com>
2018-01-16 10:46:37 +0800

    created a text to speech features

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  matches: String[];
+  isRecording = false;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, 
+    private plt: Platform, 
+    private speechRecognition: SpeechRecognition,
+    private cd: ChangeDetectorRef) {
 
   }
 
+  isIos () {
+    return this.plt.is('ios');
+  }
+
+  getPermission () {
+    this.speechRecognition.hasPermission()
+    .then((hasPermission: boolean) => {
+      if (!hasPermission) {
+        this.speechRecognition.requestPermission();
+      }
+    });
+  }
+
+  startListening () {
+    let options = {
+      language: 'en-US'
+    }
+    this.speechRecognition.startListening()
+    .subscribe(matches => {
+      this.matches = matches;
+      this.cd.detectChanges();
+    });
+    this.isRecording = true;
+  }
+
+  stopListening () {
+    this.speechRecognition.stopListening()
+    .then(() => {
+      this.isRecording = false
+    })
+  }
 }
